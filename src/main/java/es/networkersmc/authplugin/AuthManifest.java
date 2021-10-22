e24dcb9b3420be7579909cf8f7062b0e579a8671
Alberto Mimbrero <albertosm2003@gmail.com>
2021-10-22 18:36:59 +0200

    Setup injection framework

---
+++ src/main/java/es/networkersmc/authplugin/AuthManifest.java
@@ -0,0 +1,11 @@
+package es.networkersmc.authplugin;
+
+import es.networkersmc.authplugin.docs.AuthenticationData;
+import es.networkersmc.dendera.inject.Manifest;
+
+public class AuthManifest extends Manifest {
+
+    @Override
+    protected void configure() {
+    }
+}
---
+++ src/main/java/es/networkersmc/authplugin/AuthPlugin.java
@@ -0,0 +1,12 @@
+package es.networkersmc.authplugin;
+
+import es.networkersmc.dendera.DenderaInjector;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AuthPlugin extends JavaPlugin {
+
+    @Override
+    public void onLoad() {
+        DenderaInjector.getInstance().registerModule(new AuthManifest());
+    }
+}
