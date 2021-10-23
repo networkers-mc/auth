91437c63d4ec8efeca36578bea4ba634d15dcd57
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2021-10-23 19:48:36 +0200

    Add user as cache for AuthSession

--- src/main/java/es/networkersmc/authplugin/session/AuthSession.java
+++ src/main/java/es/networkersmc/authplugin/session/AuthSession.java
@@ -1,6 +1,7 @@
 package es.networkersmc.authplugin.session;
 
 import es.networkersmc.authplugin.docs.AuthenticationData;
+import es.networkersmc.dendera.docs.User;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,12 +9,14 @@ import lombok.Setter;
 @Setter
 public class AuthSession {
 
+    private final User user;
     private AuthState state;
-    private AuthenticationData data;
+    private final AuthenticationData data;
 
     private String buffer; // For REGISTER_CONFIRM and CHANGE_PASSWORD_CONFIRM states
 
-    public AuthSession(AuthState state, AuthenticationData data) {
+    public AuthSession(User user, AuthState state, AuthenticationData data) {
+        this.user = user;
         this.state = state;
         this.data = data;
     }
