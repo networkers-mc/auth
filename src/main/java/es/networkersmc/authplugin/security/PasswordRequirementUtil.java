354df9f4f7087af4e5922683aa38797e394038d9
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2021-10-23 19:48:13 +0200

    Create PasswordRequirementUtil.java

---
+++ src/main/java/es/networkersmc/authplugin/security/PasswordRequirementUtil.java
@@ -0,0 +1,32 @@
+package es.networkersmc.authplugin.security;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PasswordRequirementUtil {
+
+    public boolean isValid(String password) {
+        return len(password) && chars(password);
+    }
+
+    private boolean len(String password) {
+        return password.length() > 4 && password.length() <= 64;
+    }
+
+    private boolean chars(String password) {
+        for (char c : password.toCharArray()) {
+            if (notBetween(c, (byte) 0x21, (byte) 0x7e)         // ! -> ~
+                    && notBetween(c, (byte) 0xa1, (byte) 0xa6)  // ¡ -> |
+                    && notBetween(c, (byte) 0xbf, (byte) 0xff)  // ¿ -> ÿ
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean notBetween(char c, byte min, byte max) {
+        return c < min || c > max;
+    }
+
+}
