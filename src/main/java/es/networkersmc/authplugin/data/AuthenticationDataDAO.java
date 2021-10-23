ce11688772381c9a3d2c5b1971c93510793f6dc8
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2021-10-23 18:05:33 +0200

    Create AuthenticationDataDAO.java

---
+++ src/main/java/es/networkersmc/authplugin/data/AuthenticationDataDAO.java
@@ -0,0 +1,32 @@
+package es.networkersmc.authplugin.data;
+
+import es.networkersmc.authplugin.docs.AuthenticationData;
+import es.networkersmc.dendera.database.DatabaseCollection;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.Optional;
+import java.util.UUID;
+
+@Singleton
+public class AuthenticationDataDAO {
+
+    @Inject private DatabaseCollection<AuthenticationData> databaseCollection;
+
+    public AuthenticationData create(UUID uuid) {
+        return new AuthenticationData(uuid);
+    }
+
+    public Optional<AuthenticationData> get(UUID uuid) {
+        return Optional.ofNullable(databaseCollection.get(uuid.toString()));
+    }
+
+    public void update(AuthenticationData data) {
+        databaseCollection.update(data);
+    }
+
+    public void delete(String id) {
+        databaseCollection.delete(id);
+    }
+
+}
