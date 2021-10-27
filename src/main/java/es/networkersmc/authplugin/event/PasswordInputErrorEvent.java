42a74584e78c0b35d614a801e2363964070d92f2
Alberto Mimbrero <albertosm2003@gmail.com>
2021-10-27 11:58:36 +0200

    Make the events AuthEvent subclasses
    + Rename WrongPasswordEvent -> PasswordInputErrorEvent

--- src/main/java/es/networkersmc/authplugin/event/PasswordInputErrorEvent.java
+++ src/main/java/es/networkersmc/authplugin/event/PasswordInputErrorEvent.java
@@ -8,11 +8,11 @@ import org.bukkit.entity.Player;
  * Called when a player is registering or changing their password and the auth state changes to CONFIRM
  */
 @Getter
-public class PasswordInputEvent extends AuthEvent {
+public class PasswordInputErrorEvent extends AuthEvent {
 
     private final boolean changingPassword;
 
-    public PasswordInputEvent(Player player, AuthSession session, boolean changingPassword) {
+    public PasswordInputErrorEvent(Player player, AuthSession session, boolean changingPassword) {
         super(player, session);
         this.changingPassword = changingPassword;
     }
